9f4ca27edc1cc691212329372e7f4fbc6767c8fc
Sathya Moorthy S <sathya.sabagaja@gmail.com>
2017-12-08 13:15:44 +0530

    =Added GET /todos/:id route

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ var bodyParser = require('body-parser');
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/Todo');
 var {User} = require('./models/User');
+const {ObjectID} = require('mongodb');
 
 var app = express();
 
@@ -30,6 +31,24 @@ app.get('/todos', (req, res)=> {
     }, (err)=>{
         res.status(400).send(err);
     })
+})
+app.get('/todos/:id', (req, res)=>{
+    var id = req.params.id;
+    if(!ObjectID.isValid(id)){
+        console.log('Id is not valid')
+        res.status(404).send({})
+    }
+    Todo.findById(id).then((todo)=>{
+
+        if(!todo){
+            return res.status(404).send({})
+        }
+        res.status(200).send({todo})
+        
+    }).catch((e)=> {
+        res.status(400).send({})
+    })
+
 })
 app.listen(3000, ()=> {
     console.log("Started TodoApp on port 3000");
