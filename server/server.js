3e797376ce1b563020d5064107baebc0a3a56981
Sathya Moorthy S <sathya.sabagaja@gmail.com>
2017-12-09 18:12:46 +0530

    Added /Patch/:id route

--- server/server.js
+++ server/server.js
@@ -1,11 +1,12 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-
+const express = require('express');
+const bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
+const _ = require('lodash');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/Todo');
 var {User} = require('./models/User');
-const {ObjectID} = require('mongodb');
+
 
 const port = process.env.PORT || 3000;
 
@@ -72,6 +73,38 @@ app.delete('/todos/:id', (req, res) => {
 
     })
 })
+
+app.patch('/todos/:id', (req, res) => {
+
+    var id = req.params.id;
+    if(!ObjectID.isValid(id)){
+        return res.status(404).send({})
+    }
+
+    var body = _.pick(req.body, ['text', 'completed']);
+
+    if(_.isBoolean(body.completed) && body.completed){        
+        body.completedAt= new Date().getTime();
+    }else{
+        body.completed=false;
+        body.completedAt= null
+    }
+
+    Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then( (todo) => {
+
+        if(!todo){
+            return res.status(404).send();
+        }
+
+        res.status(200).send({todo});
+
+    }).catch((e)=>{
+        res.status(400).send();
+    })
+
+})
+
+
 app.listen(port, ()=> {
     console.log(`Started TodoApp on port ${port}`);
 })
